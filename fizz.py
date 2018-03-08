1fb13f774a3281c0301efdbed3088a5786931169
tellkeeper <tell.keeper@gmail.com>
2018-03-08 09:12:07 -0600

    Added unit testing for Mike

--- fizz.py
+++ fizz.py
@@ -1,7 +1,11 @@
+import unittest
+
 def fb(i):
 
     if i % 3 == 0 and i % 5 == 0:
         return 'FizzBuzz'
+    elif i == 100:
+        return "Bam!"
     elif i % 3 == 0:
         return 'Fizz'
     elif i % 5 == 0:
@@ -9,4 +13,15 @@ def fb(i):
     else:
         return str(i)
 
-print "\n".join(fb(n) for n in xrange(1, 100))
+class fbUnit(unittest.TestCase):
+
+    def fbUnit(self):
+        self.assertEqual(fizz(3), "Fizz")
+        self.assertEqual(fizz(5), "Buzz")
+        self.assertEqual(fizz(15), "FizzBuzz")
+        self.assertEqual(fizz(100), "Bam!")
+        self.assertEqual(fizz(1), 1)
+        self.assertEqual(fizz(4), 4)
+
+
+print "\n".join(fb(n) for n in xrange(1, 101))
